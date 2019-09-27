97f99bf35fa3fdb4c76c0cbfcf01c80a99534550
BIT <BIT@DESKTOP-08BCIT0>
2019-09-27 15:50:44 +0900

    1

--- mysite3/src/main/java/kr/co/itcen/mysite/service/Paging.java
+++ mysite3/src/main/java/kr/co/itcen/mysite/service/Paging.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.itcen.mysite.repository.BoardDao;
-import kr.co.itcen.mysite.repository.UserDao;
 
 @Service
 public class Paging {
